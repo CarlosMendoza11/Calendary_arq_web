5abd395a0e98037d9b8db22ab5e7c4566b1f880d
CarlosMendoza11 <carlos@example.com>
2026-05-14 09:11:28 -0500

    Corregir store para build en Vercel

--- calendar-frontend/src/store/store.ts
+++ calendar-frontend/src/store/store.ts
@@ -1,24 +1,24 @@
-import { configureStore, type Store } from "@reduxjs/toolkit";
-import { authSlice, calendarSlice, uiSlice } from ".";
+import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/authSlice";
+import { calendarSlice } from "./calendar/calendarSlice";
+import { uiSlice } from "./ui/uiSlice";
+
+export const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer,
+        calendar: calendarSlice.reducer,
+        ui: uiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
 
 export class StoreGlobal {
-    private static store: Store;
-
     private constructor() { }
 
-    public static getInstance(): Store {
-        if (!StoreGlobal.store) {
-            StoreGlobal.store = configureStore({
-                reducer: {
-                    auth: authSlice.reducer,
-                    calendar: calendarSlice.reducer,
-                    ui: uiSlice.reducer,
-                },
-                middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-                    serializableCheck: false,
-                }),
-            });
-        }
-        return StoreGlobal.store;
+    public static getInstance() {
+        return store;
     }
 }
